d0c0f811655ec7c8eb71a81d9a5eb9130193b94c
robin <rirobink@gmail.com>
2024-09-15 18:44:46 +0600

    fix directory

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import PatientLogin from "./components/PatientsLogin";
+
 
 export default function Home() {
   return (
     <div className="flex  bg-red-800 flex-col ">
       <p className="text-white">Hi</p>
-      <PatientLogin/>
+   
     </div>
   );
 }
