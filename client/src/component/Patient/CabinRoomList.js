38bebef1466f15adae745e73ab9c5effc4f49991
fsRakib <fsrakiba50@gmail.com>
2024-09-16 15:26:02 +0600

    Cabin Room list UI component

--- client/src/component/Patient/CabinRoomList.js
+++ client/src/component/Patient/CabinRoomList.js
@@ -2,18 +2,19 @@ import React from "react";
 import { useDataContext } from "@/context/DataContext";
 
 function MyAppointment() {
-  const { myReports } = useDataContext();
+  const { cabinRoomList } = useDataContext();
 
   return (
     <div className="h-full flex flex-col rounded-lg overflow-hidden">
       <table className="w-full table-fixed">
         <thead className=" bg-gray-700 text-white sticky top-0">
           <tr>
-            <th className="px-4 py-2 text-start w-[40%] ">Test Name</th>
-            <th className="px-4 py-2 text-start ">Result</th>
-            <th className="px-4 py-2 text-start ">Amount</th>
-            <th className="px-4 py-2 text-start ">Comment</th>
-           
+            <th className="px-4 py-2 text-start ">Name</th>
+            <th className="px-3 py-2 text-start ">Room number</th>
+            <th className="px-3 py-2 text-start ">Assigned Nurse</th>
+            <th className="px-3 py-2 text-start ">Supervising Doctor</th>
+            <th className="px-3 py-2 text-start ">Case Summary</th>
+            <th className="px-4 py-2 text-start ">Bill</th>
           </tr>
         </thead>
       </table>
@@ -21,21 +22,26 @@ function MyAppointment() {
       <div className="overflow-auto flex-grow">
         <table className="w-full bg-white table-fixed">
           <tbody>
-            {myReports.map((report, index) => (
+            {cabinRoomList.map((cabin, index) => (
               <tr key={index}>
-                <td className="border-b-2 px-4 py-2 text-start w-[40%] overflow-hidden whitespace-nowrap text-ellipsis">
-                  {report.testName}
-                </td>
                 <td className="border-b-2 px-4 py-2 text-start  overflow-hidden whitespace-nowrap text-ellipsis">
-                  {report.result}
+                  {cabin.name}
                 </td>
-                <td className="border-b-2 px-4 py-2 text-start  overflow-hidden whitespace-nowrap text-ellipsis">
-                  {report.amount}
+                <td className="border-b-2 px-3 py-2 text-start  overflow-hidden whitespace-nowrap text-ellipsis">
+                  {cabin.roomNumber}
+                </td>
+                <td className="border-b-2 px-3 py-2 text-start  overflow-hidden whitespace-nowrap text-ellipsis">
+                  {cabin.assignedNurse}
                 </td>
                 <td className="border-b-2 px-4 py-2 text-start overflow-hidden whitespace-nowrap text-ellipsis">
-                  {report.comment}
+                  {cabin.supervisingDoctor}
                 </td>
-               
+                <td className="border-b-2 px-4 py-2 text-start  overflow-hidden whitespace-nowrap text-ellipsis">
+                  {cabin.caseSummary}
+                </td>
+                <td className="border-b-2 px-5 py-2 text-start overflow-hidden whitespace-nowrap text-ellipsis">
+                  {cabin.bill}
+                </td>
               </tr>
             ))}
           </tbody>
